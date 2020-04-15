1dcdaa41a1f5793235299d87cb8be953f1d8df30
Chris Sienkiewicz <chsienki@microsoft.com>
2020-04-14 20:24:13 -0700

    WIP

--- src/Compilers/Core/Portable/SourceGeneration/GeneratorSyntaxWalker.cs
+++ src/Compilers/Core/Portable/SourceGeneration/GeneratorSyntaxWalker.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Immutable;
 
 #nullable enable
@@ -9,18 +10,31 @@ namespace Microsoft.CodeAnalysis
 {
     internal sealed class GeneratorSyntaxWalker : SyntaxWalker
     {
-        private readonly ImmutableArray<ISyntaxReceiver> _syntaxReceivers;
+        private ImmutableArray<ISyntaxReceiver> _syntaxReceivers;
+
+        private ImmutableArray<(ISyntaxReceiver, Exception)> _failedReceivers;
+
+        internal ImmutableArray<(ISyntaxReceiver receiver, Exception ex)> FailedReceivers { get => _failedReceivers; }
 
         public GeneratorSyntaxWalker(ImmutableArray<ISyntaxReceiver> syntaxReceivers)
         {
             _syntaxReceivers = syntaxReceivers;
         }
 
+
         public override void Visit(SyntaxNode node)
         {
             foreach (var receiver in _syntaxReceivers)
             {
-                receiver.OnVisitSyntaxNode(node);
+                try
+                {
+                    receiver.OnVisitSyntaxNode(node);
+                }
+                catch (Exception e)
+                {
+                    _failedReceivers = _failedReceivers.Add((receiver, e));
+                    _syntaxReceivers = _syntaxReceivers.Remove(receiver);
+                }
             }
             base.Visit(node);
         }
