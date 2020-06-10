17e80f0ec405abe8a639a957d3e2a0ebfa150edb
Chris Sienkiewicz <chsienki@microsoft.com>
2020-06-10 12:54:04 -0700

    WIP

---
+++ src/Compilers/CSharp/Portable/Errors/ExperimentalAttribute.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#nullable enable
+
+/// <summary>
+/// PROTOTYPE: add explaination
+/// </summary>
+
+namespace Windows.Foundation.Metadata
+{
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
+    internal sealed class ExperimentalAttribute : Attribute
+    {
+    }
+}
